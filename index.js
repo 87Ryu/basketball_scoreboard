baa8cf1844e99f18c327c550748a6e59e57172cd
87Ryu <radv87@gmail.com>
2026-02-22 22:35:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function add1(team = 'home') {
+    const scoreElement = document.getElementById(`${team}-score`);
+    let currentScore = parseInt(scoreElement.textContent);
+    scoreElement.textContent = currentScore + 1;
+}
+
+function add2(team = 'home') {
+    const scoreElement = document.getElementById(`${team}-score`);
+    let currentScore = parseInt(scoreElement.textContent);
+    scoreElement.textContent = currentScore + 2;
+}
+
+function add3(team = 'home') {
+    const scoreElement = document.getElementById(`${team}-score`);
+    let currentScore = parseInt(scoreElement.textContent);
+    scoreElement.textContent = currentScore + 3;
+}
+
+function newGame() {
+    document.getElementById('home-score').textContent = 0;
+    document.getElementById('guest-score').textContent = 0;
+}
